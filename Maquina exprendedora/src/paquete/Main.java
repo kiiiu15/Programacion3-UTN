db8adbba76c720e7465044cace1e043a1117dd0b
kiiiu15 <kiiiu15@users.noreply.github.com>
2019-04-09 10:15:24 -0300

    BORRON Y CUENTA NUEVA
    ESCRIBAS LA HARCODEADA CONCHA DE TU MADRE!!!

--- Maquina exprendedora/src/paquete/Main.java
+++ Maquina exprendedora/src/paquete/Main.java
@@ -1,11 +1,10 @@
-package paquetovich;
+package paquete;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Venta hola= new Venta();
-	
+			System.out.println("ESCRIBAS LA HARCODEADA CONCHA DE TU MADRE!!!");
 	}
 
 }
