1143e220990e7f634f5b2059063b3e01794f215c
Kiiiu15 <kiiiu15@users.noreply.github.com>
2019-05-09 12:26:51 -0300

    Prueba de Interfaz
    Se impieza con InAndOut y se ve como se puede ir acoplando con Swing

---
+++ Prueba con Interfaz grafica/src/paquete/Formulario.java
@@ -0,0 +1,9 @@
+package paquete;
+import javax.swing.*;
+import java.awt.Event.*;
+import java.util.EventListener;
+import java.util.ArrayList;
+
+public class Formulario extends JFrame implements EventListener{
+	
+}
