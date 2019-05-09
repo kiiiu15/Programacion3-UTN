749c7e19fe72b12bf0abb4d0484ec44518ad2584
Kiiiu15 <kiiiu15@users.noreply.github.com>
2019-05-09 12:59:54 -0300

    1ra aproximacion

--- Prueba con Interfaz grafica/src/paquete/Formulario.java
+++
@@ -1,9 +0,0 @@
-package paquete;
-import javax.swing.*;
-import java.awt.Event.*;
-import java.util.EventListener;
-import java.util.ArrayList;
-
-public class Formulario extends JFrame implements EventListener{
-	
-}
---
+++ Prueba con Interfaz grafica/src/paquete/Pantalla.java
@@ -0,0 +1,15 @@
+package paquete;
+import javax.swing.*;
+import java.awt.Event.*;
+import java.util.EventListener;
+import java.util.ArrayList;
+
+public class Pantalla extends JFrame implements EventListener{
+	private ArrayList<JButton> botonesDelFormulario;
+	private ArrayList<JTextField> lineasDeTextoDelFormulario;//cambiar nombre por favor! o no.......
+	private ArrayList<JTextArea> cuadrosDeTextosDelFormulario;
+	private ArrayList<JLabel> checksDelFormulario;
+	private ArrayList<JCheckBox>  checkBoxsDelFormulario;
+	
+
+}
